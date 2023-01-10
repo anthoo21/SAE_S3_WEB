79747ce92d702632dfedf9ed4bd8e0dc4d9b6a5e
Marion BRUEL <90557768+marionbruel81@users.noreply.github.com>
2023-01-10 11:01:20 +0100

    Modification script

--- www/cabinet_medical/scriptsql/medsoft.sql
+++ www/cabinet_medical/scriptsql/medsoft.sql
@@ -51,7 +51,7 @@ INSERT INTO utilisateurs VALUES ('chaber90', 'apM#é!Kp', 'MED');
 
 -- 3 Structure table medecins
 CREATE TABLE `medecins` (
-  `id_med` CHAR(3) NOT NULL,
+  `id_med` INT(3) NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(25) NOT NULL,
   `prenom` VARCHAR(25) NOT NULL,
   `dateNai` DATE NOT NULL, 
@@ -74,11 +74,12 @@ CREATE TABLE `patients` (
   `nom` VARCHAR(25) NOT NULL,
   `prenom` VARCHAR(25) NOT NULL,
   `id_genre` CHAR(2) NOT NULL,
+  `adresse` VARCHAR(50) NOT NULL,
   `tel` CHAR(10) NOT NULL,
   `email` VARCHAR(40),
   `dateNai` DATE NOT NULL,
   `poids` NUMERIC(6,3) NOT NULL,
-  `id_medecin` CHAR(3) NOT NULL,
+  `id_medecin` INT(3) NOT NULL,
   `allergies` CHAR(3) NOT NULL,
   `commentaires` LONGBLOB,
   PRIMARY KEY(numeroCarteVitale),
@@ -87,17 +88,17 @@ CREATE TABLE `patients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Insertion des patients
-INSERT INTO patients VALUES ('180088100412100', 'DUPOND', 'Patrick', '02', '0636465666', 'patrickdupond@gmail.com', '1980-08-12', '082.500', '001', 'non', 
+INSERT INTO patients VALUES ('180088100412100', 'DUPOND', 'Patrick', '02', '4 avenue Colonel Teyssier 81000 Albi','0636465666', 'patrickdupond@gmail.com', '1980-08-12', '082.500', '001', 'non', 
 'Atteint de diabète de type II depuis l age de 6 ans. Consultation tous les 2 mois maximum pour les prescriptions du diabète. Fracture de la cheville gauche en 2020. Non-fumeur.');
-INSERT INTO patients VALUES ('203118100403710', 'BONNET', 'Julie', '01', '0611223344', 'juliebonnet@gmail.com', '2003-11-01', '071.658', '003', 'non', 'Petits problemes respiratoire. Antécédents de diabète et de cancer dans la famille. 3 doses anticovid.');
-INSERT INTO patients VALUES ('103068002154853', 'MONCZEWSKI', 'Gabriel', '02', '0781828384', 'gabrielmonczewski@gmail.com', '2003-06-14', '088.432', '002', 'non', 'Ablation des végétations et des amygdales. 3 doses anticovid + covid');
+INSERT INTO patients VALUES ('203118100403710', 'BONNET', 'Julie', '01', '31 avenue de Bordeaux 12000 Rodez','0611223344', 'juliebonnet@gmail.com', '2003-11-01', '071.658', '003', 'non', 'Petits problemes respiratoire. Antécédents de diabète et de cancer dans la famille. 3 doses anticovid.');
+INSERT INTO patients VALUES ('103068002154853', 'MONCZEWSKI', 'Gabriel', '02', '17 rue Béteille 12000 Rodez', '0781828384', 'gabrielmonczewski@gmail.com', '2003-06-14', '088.432', '002', 'non', 'Ablation des végétations et des amygdales. 3 doses anticovid + covid');
 
 -- 5 Structure table visites
 CREATE TABLE `visites` (
-  `id_visite` CHAR(4) NOT NULL,
+  `id_visite` INT(4) NOT NULL AUTO_INCREMENT,
   `date_visite` DATE NOT NULL,
   `id_patient` CHAR(15) NOT NULL,
-  `id_medecin` CHAR(3) NOT NULL,
+  `id_medecin` INT(3) NOT NULL,
   `motif` VARCHAR(50) NOT NULL,
   `observations` LONGBLOB,
   PRIMARY KEY(id_visite),
@@ -106,28 +107,28 @@ CREATE TABLE `visites` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Insertion des visites
-INSERT INTO visites VALUES ('V001', '2022-10-18', '180088100412100', '001', 'Surveillance diabete', 'Le patient régule très bien son diabete lors de phases d hypo ou d hyperglycémie. Quelques phases de baisses de sucres pendant la nuit.');
-INSERT INTO visites VALUES ('V002', '2022-09-23', '203118100403710', '003', 'Vaccin papillon', 'Aucune reaction negativve');
-INSERT INTO visites VALUES ('V003', '2022-04-25', '103068002154853', '002', 'Intoxication alimentaire', 'Le patient présente des maux de ventre, des nauxsées ainsi que des maux de tête');
+INSERT INTO visites VALUES ('0001', '2022-10-18', '180088100412100', '001', 'Surveillance diabete', 'Le patient régule très bien son diabete lors de phases d hypo ou d hyperglycémie. Quelques phases de baisses de sucres pendant la nuit.');
+INSERT INTO visites VALUES ('0002', '2022-09-23', '203118100403710', '003', 'Vaccin papillon', 'Aucune reaction negativve');
+INSERT INTO visites VALUES ('0003', '2022-04-25', '103068002154853', '002', 'Intoxication alimentaire', 'Le patient présente des maux de ventre, des nauxsées ainsi que des maux de tête');
 
 -- 6 Structure table ordonnances
 CREATE TABLE `ordonnances` (
-  `id_ordo` CHAR(9) NOT NULL,
-  `id_visite` CHAR(4) NOT NULL,
+  `id_ordo` INT(5) NOT NULL AUTO_INCREMENT,
+  `id_visite` INT(4) NOT NULL,
   PRIMARY KEY(id_ordo),
   CONSTRAINT fk_ordonnances_visites FOREIGN KEY(id_visite) REFERENCES visites(id_visite)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Insertion des ordonnances
-INSERT INTO ordonnances VALUES ('ORD000001', 'V001');
-INSERT INTO ordonnances VALUES ('ORD000002', 'V002');
-INSERT INTO ordonnances VALUES ('ORD000003', 'V003');
+INSERT INTO ordonnances VALUES ('00001', '0001');
+INSERT INTO ordonnances VALUES ('00002', '0002');
+INSERT INTO ordonnances VALUES ('00003', '0003');
 
 -- 7 Structure table prescriptions
 CREATE TABLE `prescriptions` (
-	`id_prescriptions` CHAR(9) NOT NULL,
-	`id_ordonnance` CHAR(9) NOT NULL,
-	`id_medicaments` CHAR(3) NOT NULL,
+	`id_prescriptions` INT(6) NOT NULL,
+	`id_ordonnance` INT(5) NOT NULL,
+	`id_medicaments` INT(8) NOT NULL,
 	`posologie` VARCHAR(50) NOT NULL,
 	PRIMARY KEY(id_prescriptions),
 	CONSTRAINT fk_prescriptions_ordonnances FOREIGN KEY(id_ordonnance) REFERENCES ordonnances(id_ordo)
@@ -135,6 +136,6 @@ CREATE TABLE `prescriptions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Insertion des prescriptions
-INSERT INTO prescriptions VALUES ('PRE000001', 'ORD000001', 'AB1', '3 fois par jour');
-INSERT INTO prescriptions VALUES ('PRE000002', 'ORD000002', 'AB2', '1 comprimé toutes les 4h');
-INSERT INTO prescriptions VALUES ('PRE000003', 'ORD000002', 'AB3', '1 comprimé tous les soirs');
+INSERT INTO prescriptions VALUES ('000001', '00001', '65089833', '3 fois par jour');
+INSERT INTO prescriptions VALUES ('000002', '00002', '60904643', '1 comprimé toutes les 4h');
+INSERT INTO prescriptions VALUES ('000003', '00002', '66796142', '1 comprimé tous les soirs');
